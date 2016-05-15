49fc3953f5b9c55d72fa2d45aa5931a6e4066a44
Cem <kayacem@gmx.de>
2016-05-15 14:58:37 +0200

    new Function

--- FirstGit/src/test/Start.java
+++ FirstGit/src/test/Start.java
@@ -4,6 +4,13 @@ public class Start {
 
 	public static void main (String[] args){
 		
-		System.out.println("Hello World!");
+		new Start().sayHello();
 	}
+	
+	public void sayHello(){
+		System.out.println("HelloWorld!");
+	}
+	
+	
 }
+
